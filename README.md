189158a4d3d7b81d4404347ced8e78bb1a7b8114
JackShiu <poming.shiu@gmail.com>
2023-10-26 12:13:25 +0800

    initialize project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Highstreet SmartContract Project
+
+This project includes entirely highstreet smartContracts
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.ts
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,51 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 9999,
+        details: {
+          yul: true,
+        },
+      },
+    },
+  },
+    networks: {
+    mainnet: {
+      url: process.env.ENDPOINT_MAINNET || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    sepolia: {
+      url: process.env.ENDPOINT_SEPOLIA || "",
+      accounts: process.env.PRIVATE_KEY_TESTNET !== undefined ? [process.env.PRIVATE_KEY_TESTNET] : [],
+    },
+    goerli: {
+      url: process.env.ENDPOINT_GOERLI || "",
+      accounts: process.env.PRIVATE_KEY_TESTNET !== undefined ? [process.env.PRIVATE_KEY_TESTNET] : [],
+    },
+    bsc:  {
+      url: process.env.ENDPOINT_BSC || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 56
+    },
+    bscTestnet:  {
+      url: process.env.ENDPOINT_BSC_TESTNET || "",
+      accounts: process.env.PRIVATE_KEY_TESTNET !== undefined ? [process.env.PRIVATE_KEY_TESTNET] : [],
+      chainId: 97
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  mocha: {
+    timeout: 3000000,
+  },
+};
+
+export default config;
